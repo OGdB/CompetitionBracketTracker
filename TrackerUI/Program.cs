5d619a94e6d539a2d5aa3018f4052fdd725983c4
OGdB <58260499+OGdB@users.noreply.github.com>
2023-08-12 16:33:48 +0200

    Put forms in folder

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -8,10 +8,8 @@ namespace TrackerUI
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new TournamentViewerForm());
+            Application.Run(new TournamentDashboardForm());
         }
     }
 }
